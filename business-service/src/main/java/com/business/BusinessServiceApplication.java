68eead1c3d044a5313d191111bf817b7bf2ee236
miguel robledo <stmiguelangelgarciarobledo@gmail.com>
2026-02-16 15:19:13 -0500

    Arreglo de dockerizaciomn de microservicios

--- business-service/src/main/java/com/business/BusinessServiceApplication.java
+++ business-service/src/main/java/com/business/BusinessServiceApplication.java
@@ -1,4 +1,4 @@
-package com.business.business_service;
+package com.business;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BusinessServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BusinessServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessServiceApplication.class, args);
+    }
 }
+
